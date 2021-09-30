28fd10387f1c89817c5e140cd8a17ace183625b0
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-30 08:31:43 -0400

    got some testing in the index

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import './style.css';
 import makeToDoItem from './makeItem';
 import checkForProject from './checkForProject';
 import importToDoItemToDom from './importItem';
+
+function start () {
+    const item = makeToDoItem("title input", "this is a description input", "Due Date Now", "High priority", "super confused and just a test", ["finish", "this", "project"]);
+
+}

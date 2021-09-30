6e7508822ffd16021fd1f3ed6a331ae40d493c97
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-30 07:46:15 -0400

    made a module that makes an todo item function factory

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import './style.css';
-console.log("hello world");
+import makeToDoItem from './makeItem';
---
+++ src/makeItem.js
@@ -0,0 +1,4 @@
+function makeToDoItem (title, description, dueDate, priority, notes, checklist) {   
+    return {  title, description, dueDate, priority, notes, checklist};
+}
+export default makeToDoItem;

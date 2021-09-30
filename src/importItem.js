20df8699f22efa72c9d13ea0f089f4d6259aeaa0
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-30 12:04:24 -0400

    finished the overall dom applying, we now can apply items to the dom

--- src/importItem.js
+++ src/importItem.js
@@ -24,16 +24,22 @@ console.log(projectDiv);
                 itemHeader.classList.add("Item-Header");
                 itemTextDiv.appendChild(itemHeader);
                     const headerH3 = document.createElement("h3");
+                    headerH3.textContent = item.title;
                     itemHeader.appendChild(headerH3);
                     const displayItemsBtn = document.createElement("button");
+                    displayItemsBtn.classList.add("display-items");
+                    displayItemsBtn.textContent = "Press this to hide the content below";
                     itemHeader.appendChild(displayItemsBtn);
                 /*END HEADER DIV */
                 /*Due-Date, Priority, description */
                 const dueDateh3 = document.createElement("h3");
+                dueDateh3.textContent = item.dueDate;
                 itemTextDiv.appendChild(dueDateh3);
                 const priorityh4 = document.createElement("h4");
+                priorityh4.textContent = item.priority;
                 itemTextDiv.appendChild(priorityh4);
                 const descriptionP = document.createElement("p");
+                descriptionP.textContent = item.description;
                 itemTextDiv.appendChild(descriptionP);
                 /*END Due-Date, Priority, description */
                 /*BEGIN Check list */
@@ -42,13 +48,21 @@ console.log(projectDiv);
                 itemTextDiv.appendChild(checkListDiv);
                     const listNoteP = document.createElement("p");
                     listNoteP.classList.add("list-note");
+                    listNoteP.textContent = "Your check list for this To-Do-List:"
                     checkListDiv.appendChild(listNoteP);
                     /*Un-ordered list begin */
                     const checklistUL = document.createElement("ul");
                     //for loop that loops through checklist and lists them
+                    for(let i = 0; i < item.checklist.length; i++)
+                    {
+                        let newlistItem = document.createElement("li");
+                        newlistItem.textContent = item.checklist[i];
+                        checklistUL.appendChild(newlistItem);
+                    }
                     /* END unordered list*/
                     itemTextDiv.appendChild(checklistUL);
                 const additionalNotesP = document.createElement("p");
+                additionalNotesP.textContent = item.notes;
                 itemTextDiv.appendChild(additionalNotesP);
     }
 }

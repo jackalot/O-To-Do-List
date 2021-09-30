75852c689c93a48b86bfb5a70ee6fb10a49aa4a2
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-30 09:21:39 -0400

    now checks if the project is available and calls importToDoItemToDom

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ let projects = ["test1", "test2"];
 function start () {
     const item = makeToDoItem("title input", "this is a description input", "Due Date Now", "High priority", "super confused and just a test", ["finish", "this", "project"]);
     console.log(item);
-    const projectIsAvailable = checkForProject("ddsd", projects);
+    const projectIsAvailable = checkForProject("test1", projects);
     console.log(projectIsAvailable);
-
+    if(projectIsAvailable) //its not false so it returned one of the project strings
+    {
+        importToDoItemToDom();
+    }
 }
 start();

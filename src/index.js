8e9b56ce44a37524be671e7ab62498f9c4681b32
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-05 11:55:49 -0400

    now you can add a project wiotha  button click

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import checkForProject from './checkForProject';
 import importToDoItemToDom from './importItem';
 import CreateProject from './createAProject';
 let projects = ["test1", "test2"]; //since I am too lazy to refactor modules to work with objects
-function start () {
+
+const addContentBtn = document.querySelector("button");
+addContentBtn.addEventListener("click", () => {
     const projectInput = window.prompt("Which project is this to do item? (if it is a new project, just insert the name)");
     const titleInput = window.prompt("What is the title of your to-do item?");
     const descInput = window.prompt("Whats the description for it?");
@@ -12,11 +14,14 @@ function start () {
     const dudeDateInput = window.prompt("When is this due?")
     const notesInput = window.prompt("Any additional notes?");
     const checkListInput = window.prompt("For the checklist, insert one of the things you want in there?");
-    //const item = makeToDoItem(titleInput, descInput, dudeDateInput, priorityInput, notesInput, [checkListInput]); // actual use
-    const item = makeToDoItem("titleInput", "descInput", "dudeDateInput", "priorityInput", "notesInput", ["checkListInput"]); //debugging
+    start (projectInput, titleInput, descInput, priorityInput, dudeDateInput, notesInput, checkListInput);
+})
+function start (projectInput, titleInput, descInput, priorityInput, dudeDateInput, notesInput, checkListInput) {
+    const item = makeToDoItem(titleInput, descInput, dudeDateInput, priorityInput, notesInput, [checkListInput]); // actual use
+    //const item = makeToDoItem("titleInput", "descInput", "dudeDateInput", "priorityInput", "notesInput", ["checkListInput"]); //debugging
     //console.log(item);
-    //const projectIsAvailable = checkForProject(projectInput, projects);
-    const projectIsAvailable = checkForProject("projectInput", projects); //debugging
+    const projectIsAvailable = checkForProject(projectInput, projects);
+    //const projectIsAvailable = checkForProject("projectInput", projects); //debugging
     //console.log(projectIsAvailable);
     if(projectIsAvailable) //its not false so it returned one of the project strings
     {
@@ -26,10 +31,10 @@ function start () {
     {
         //console.log("creating project");
         //Note = make sure before passing in value theres no spaces
-       //CreateProject(projectInput); //actual use
-       CreateProject("projectInput"); //debugging
-       //importToDoItemToDom(item, projectInput) //actual use
+       CreateProject(projectInput); //actual use
+       //CreateProject("projectInput"); //debugging
+       importToDoItemToDom(item, projectInput) //actual use
        importToDoItemToDom(item, "projectInput");//debugging
     }
 }
-start();
+//start();

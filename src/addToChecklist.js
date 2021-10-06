66bd59b5a0a8dea6bffc38aaebe65202f026e29b
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-06 09:58:31 -0400

    addToCheckList now removes the list elements before adding new ones

--- src/addToChecklist.js
+++ src/addToChecklist.js
@@ -1,15 +1,22 @@
 //myChecklist is the UL that holds all the list items, item is our TO-Do-Item
 function addToCheckList (myCheckList, item) {
     const newListItem = window.prompt("what do you want to add to this checklist? (you have to click the button again if you want more than 1)");
-    item.checklist.push(newlistItem);
+    item.checklist.push(newListItem);
     console.log(item.checklist);
     //ADD empty checklist here
-
-    for(let i = 0; i < item.checklist.length; i++)
+    while(myCheckList.firstChild)
     {
-        let newlistItem = document.createElement("li");
-        newlistItem.textContent = item.checklist[i];
-        myCheckList.appendChild(newlistItem);
+        console.log(myCheckList.firstChild);
+        myCheckList.removeChild(myCheckList.firstChild);
+    }
+    if(!myCheckList.firstChild)
+    {
+        for(let i = 0; i < item.checklist.length; i++)
+        {
+            let newlistItem = document.createElement("li");
+            newlistItem.textContent = item.checklist[i];
+            myCheckList.appendChild(newlistItem);
+        }
     }
 }
 export default addToCheckList;

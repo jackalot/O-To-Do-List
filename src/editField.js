f7ee049dc5fcad5ac6659d5e06bda4346e816e85
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-08 09:37:25 -0400

    Started new branch, refactor: refactor all code

--- src/editField.js
+++
@@ -1,8 +0,0 @@
-//Edit each field,                               //ElementType is for the item object so we can change it depending on the field we're on
-function editField(element, promptMessage, item, elementType) {
-const input = window.prompt(promptMessage);
-element.textContent = input;
-item[elementType] = input; //work on this
-console.log(item);
-}
-export default editField;

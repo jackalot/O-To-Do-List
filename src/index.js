1f1c92ff393677563cedc0eb27afcd1234d061a5
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-11 11:14:21 -0400

    changed the entire form dom manipulation into a module pattern

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
-const OpenBtn = document.querySelector(".open-button");
-//console.log({OpenBtn});
-OpenBtn.addEventListener("click", () => {
-    openForm();
-});
-const form  = document.querySelector(".form-popup");
-form.style.display = "none";
-function openForm() {
- form.style.display = "block";
-}
-function closeForm() {
+//this module pattern will handle manipulating the form when it comes to the dom
+const formDomManupulate = (function(doc) {
+    const OpenBtn = doc.querySelector(".open-button");
+    OpenBtn.addEventListener("click", () => {
+        openForm();
+    });
+    const form  = document.querySelector(".form-popup");
     form.style.display = "none";
-}
-const selectProjectMenu = document.querySelector(".Project-Name");//The actual menu
-const selectProjectOptions = selectProjectMenu.childNodes;
-const newProjectInput = document.querySelector(".new-project");
-newProjectInput.style.display = "none";
-selectProjectMenu.addEventListener('click', () => {
-    AddProjElement();
-})
-function AddProjElement() { //when a select project element is clicked, it triggers this function
-    if(selectProjectMenu.value === "NewProject")
-    {
-     newProjectInput.style.display = "block";
+
+    function openForm() {
+     form.style.display = "block";
     }
-    else
-    {
-        newProjectInput.style.display = "none";
+
+    const closeBtn = doc.querySelector(".btn-cancel");
+    closeBtn.addEventListener("click", () => {
+        closeForm();
+    });
+    function closeForm() {
+        form.style.display = "none";
     }
-}
+    const selectProjectMenu = document.querySelector(".Project-Name");//The actual menu
+    const newProjectInput = document.querySelector(".new-project");
+    newProjectInput.style.display = "none";
+    selectProjectMenu.addEventListener('click', () => {
+        AddProjElement();
+    })
+    function AddProjElement() { //when a select project element is clicked, it triggers this function
+        if(selectProjectMenu.value === "NewProject")
+        {
+         newProjectInput.style.display = "block";
+        }
+        else
+        {
+            newProjectInput.style.display = "none";
+        }
+    }
+})(document);

f45583baf42583389a910369dce273b7dd0e195d
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-12 09:50:50 -0400

    fixed a typo, added a class to the checklist UL, and made a variable which queryselects it

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const formDomManupulate = (function(doc) {
     const OpenBtn = doc.querySelector(".open-button");
     OpenBtn.addEventListener("click", openForm);
     //Grab the form itself, and hide it
-    const form  = document.querySelector(".form-popup");
+    const form  = doc.querySelector(".form-popup");
     form.style.display = "none";
 
     function openForm() {
@@ -16,16 +16,16 @@ const formDomManupulate = (function(doc) {
         form.style.display = "none";
     }
     //grab the project menu and the field, set the field to none by default
-    const selectProjectMenu = document.querySelector(".Project-Name");//The actual menu
-    const newProjectInput = document.querySelector(".new-project"); //The field that adds a new project
+    const selectProjectMenu = doc.querySelector(".Project-Name");//The actual menu
+    const newProjectInput = doc.querySelector(".new-project"); //The field that adds a new project
     newProjectInput.style.display = "none";
     //Every click on the select menu will call the AddProjElement(); 
     selectProjectMenu.addEventListener('click', () => {
         AddNewElement(selectProjectMenu, newProjectInput, "NewProject");
     })
     //Same thing as the project menu, but for the todolist title
-    const selectTitleMenu = document.querySelector(".To-Do-List-title");//The actual menu
-    const newTitleInput = document.querySelector(".New-To-Do-List-title"); //The field that adds a new title
+    const selectTitleMenu = doc.querySelector(".To-Do-List-title");//The actual menu
+    const newTitleInput = doc.querySelector(".New-To-Do-List-title"); //The field that adds a new title
     newTitleInput.style.display = "none";
     //Every click on the select menu will call the AddTitleElement(); 
     selectTitleMenu.addEventListener('click', () => {
@@ -42,4 +42,6 @@ const formDomManupulate = (function(doc) {
             inputElement.style.display = "none";
         }
     }
+    const checklistUl = doc.querySelector("To-Do-List-checklist"); //the UL element that holds the checklist
+    
 })(document);

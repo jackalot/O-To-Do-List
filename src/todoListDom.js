67c475ea2c6f7227dc5a01b6059f3a50f7da2b47
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-21 08:03:13 -0400

    added a new naming scheme, tried to call the module from index.js, tried to display Project titles on the new module

--- src/todoListDom.js
+++ src/todoListDom.js
@@ -1,6 +1,17 @@
 //adds items to the todolist
 const toDoListAddToDom = (() => {
-    
-    console.log("hi");
-})();
+    const toDoList = document.querySelector(".To-Do-List");
+   function displayProjectTitle () {
+        let Retrieved = localStorage.getItem("projectTitles"); //create a Retrieved variable from local storage
+        let projectTitles = JSON.parse(Retrieved); //parse it into a javascript array we can use
+        if(projectTitles !== null)
+        {
+            for(let i=0; i < projectTitles.length; i++)
+            {
+                toDoList.append(projectTitles[i]);
+            }
+        }
+    }
+    return { displayProjectTitle };
+})(document);
 export default toDoListAddToDom;

afe042612fd753485a4d4a95c651455c8a98d9eb
jackalotUVM <gonzalezc9999@gmail.com>
2021-10-29 11:31:38 -0400

    can now store an object and retrieve it

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("function CreateProject (projectName, toDoListTitle, dueDate, priority, notes, checklist)\n{\n    localStorage.setItem(`${projectName}`, projectName);\n    localStorage.setItem(`${projectName}-${toDoListTitle}`, toDoListTitle);\n    localStorage.setItem(`${projectName}-${dueDate}`, dueDate);\n    localStorage.setItem(`${projectName}-${priority}`, priority);\n    localStorage.setItem(`${projectName}-${notes}`, notes);\n    localStorage.setItem(`${projectName}-${checklist}`, checklist);\n    return { projectName, toDoListTitle, dueDate, priority, notes, checklist }\n}\nCreateProject(\"project\", \"new title\", \"new dudedate\", \"new priority\", \"notes are cool\", [\"checklist item one\", \"checklist item 2\"]);\n\n//# sourceURL=webpack://O-To-Do-List/./src/index.js?");
+eval("function CreateProject (projectName, toDoListTitle, dueDate, priority, notes, checklist)\n{\n    return { projectName, toDoListTitle, dueDate, priority, notes, checklist }\n}\nconst newObj = CreateProject(\"project\", \"new title\", \"new dudedate\", \"new priority\", \"notes are cool\", [\"checklist item one\", \"checklist item 2\"]);\nlocalStorage.setItem(`${newObj.projectName}`, JSON.stringify(newObj));\nlet retrieved = localStorage.getItem(`${newObj.projectName}`);\nlet objectAfter = JSON.parse(retrieved);\nconsole.log(objectAfter.projectName);\n\n//# sourceURL=webpack://O-To-Do-List/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 function CreateProject (projectName, toDoListTitle, dueDate, priority, notes, checklist)
 {
-    localStorage.setItem(`${projectName}`, projectName);
-    localStorage.setItem(`${projectName}-${toDoListTitle}`, toDoListTitle);
-    localStorage.setItem(`${projectName}-${dueDate}`, dueDate);
-    localStorage.setItem(`${projectName}-${priority}`, priority);
-    localStorage.setItem(`${projectName}-${notes}`, notes);
-    localStorage.setItem(`${projectName}-${checklist}`, checklist);
     return { projectName, toDoListTitle, dueDate, priority, notes, checklist }
 }
-CreateProject("project", "new title", "new dudedate", "new priority", "notes are cool", ["checklist item one", "checklist item 2"]);
+const newObj = CreateProject("project", "new title", "new dudedate", "new priority", "notes are cool", ["checklist item one", "checklist item 2"]);
+localStorage.setItem(`${newObj.projectName}`, JSON.stringify(newObj));
+let retrieved = localStorage.getItem(`${newObj.projectName}`);
+let objectAfter = JSON.parse(retrieved);
+console.log(objectAfter.projectName);

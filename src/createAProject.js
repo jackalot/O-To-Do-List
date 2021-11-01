138c5246d2f0cfe29e4229b882d40b3d5fb74597
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-01 10:13:09 -0400

    createAProject is now seperated into multiple functions, results in the same form but now we have an object we can log to console to see the result

--- src/createAProject.js
+++ src/createAProject.js
@@ -1,27 +1,51 @@
-//when you search for a project that doesnt exist
-//we call this module
+//creates the project DOM elements and appends them
 import hideProject from './hideProject';
 function CreateProject (projectName) {
-    const projectDiv = document.createElement("div");
-    projectDiv.classList.add("Project-div");
-    projectDiv.id = localStorage.getItem(`${projectName}`);
-    const body = document.querySelector("body");
-    body.appendChild(projectDiv);
+    //creates the projectDiv and appends it to the body
+    function createProjectDiv(projectName) {
+        const wholeDocument = {};
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("Project-div");
+        projectDiv.id = localStorage.getItem(`${projectName}`);
+        wholeDocument.projectDiv = projectDiv;
+        const body = document.querySelector("body");
+        body.appendChild(projectDiv);
+        wholeDocument.body = body;
+        wholeDocument.projectName = projectName;
+        createProjectHeader(wholeDocument)
+    }
+    createProjectDiv(projectName);
+    function createProjectHeader(wholeDocument) {
         const projectHeaderDiv = document.createElement("div");
         projectHeaderDiv.classList.add("project-header");
-        projectDiv.appendChild(projectHeaderDiv);
-            const H2Name = document.createElement("h2");
-            H2Name.textContent = localStorage.getItem(`${projectName}`);
-            const displayBtn = document.createElement("button");
+        wholeDocument.projectHeaderDiv = projectHeaderDiv;
+        wholeDocument.projectDiv.appendChild(projectHeaderDiv);
+        createProjectHeadingName(wholeDocument);
+    }
+    function createProjectHeadingName(wholeDocument) {
+        const H2Name = document.createElement("h2");
+            H2Name.textContent = localStorage.getItem(`${wholeDocument.projectName}`);
+            wholeDocument.H2Name = H2Name;
+            wholeDocument.projectHeaderDiv.appendChild(H2Name);
+            createProjectDisplayButton(wholeDocument);
+    }
+    function createProjectDisplayButton(wholeDocument) {
+        const displayBtn = document.createElement("button");
             displayBtn.textContent = "Display Project To-Do-List";
             displayBtn.addEventListener('click', ()=> {
                 console.log("click");
-                hideProject(localStorage.getItem(`${projectName}`));
+                hideProject(localStorage.getItem(`${wholeDocument.projectName}`));
             });
             displayBtn.classList.add("display-items");
-            projectHeaderDiv.appendChild(H2Name);
-            projectHeaderDiv.appendChild(displayBtn);
+            wholeDocument.displayBtn = displayBtn;
+            wholeDocument.projectHeaderDiv.appendChild(displayBtn);
+            createProjectUL(wholeDocument);
+    }
+    function createProjectUL (wholeDocument) {
         const ul = document.createElement("ul");
-        projectDiv.appendChild(ul);
+        wholeDocument.ul = ul;
+        wholeDocument.projectDiv.appendChild(ul);
+        console.log(wholeDocument);
+    }
 }
 export default CreateProject;

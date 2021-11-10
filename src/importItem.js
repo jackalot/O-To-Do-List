188b89260b67a23bf789cc69fb0064d0c26e3feb
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-10 08:51:51 -0500

    added a 'createEditNotedBtn', added additional null checking to 'createRemoveFromChecklistBtn'

--- src/importItem.js
+++ src/importItem.js
@@ -294,38 +294,57 @@ const projectUL = document.querySelector(`#${joinedProjectName}-ul`);
         function createRemoveFromChecklistBtn () {
             const checklistUl = document.querySelector(`#${joinedProjectName}-${item.ToDoListtitle}-checkList-Ul`);
             const itemBoxDiv = document.querySelector(`#${joinedProjectName}-${item.ToDoListtitle}-ItemBox`);
-            if(checklistUl === null)
+            if(itemBoxDiv === null)
             {
-                createChecklistUL();
+                createItemBoxDiv()
                 createRemoveFromChecklistBtn();
             }
             else
             {
-                //make a button that adds items to the checklist
-                const removeFromChecklistBtn = createNewElement("button", "remove-from-check-list", itemBoxDiv, null, `${joinedProjectName}-${item.ToDoListtitle}-addToChecklistBtn`)
-                removeFromChecklistBtn.textContent = "Remove From checklist";
-                removeFromChecklistBtn.addEventListener('click', () => {
-                    removeCheckListItem(checklistUL, item);
+                if(checklistUl === null)
+                {
+                    createChecklistUL();
+                    createRemoveFromChecklistBtn();
+                }
+                else
+                {
+                    //make a button that adds items to the checklist
+                    const removeFromChecklistBtn = createNewElement("button", "remove-from-check-list", itemBoxDiv, null, `${joinedProjectName}-${item.ToDoListtitle}-addToChecklistBtn`)
+                    removeFromChecklistBtn.textContent = "Remove From checklist";
+                    removeFromChecklistBtn.addEventListener('click', () => {
+                        removeCheckListItem(checklistUL, item);
+                    })
+                }
+            }
+        }
+        function createEditNotesBtn () {
+            const itemBoxDiv = document.querySelector(`#${joinedProjectName}-${item.ToDoListtitle}-ItemBox`);
+            if(itemBoxDiv === null)
+            {
+                createItemBoxDiv()
+                createRemoveFromChecklistBtn();
+            }
+            else
+            {
+                const editNotesBtn = createNewElement("button", "edit-notes", itemBoxDiv, null, `${joinedProjectName}-${item.ToDoListtitle}-editNotesBtn`)
+                editNotesBtn.textContent = "Edit Additional Notes";
+                editNotesBtn.addEventListener('click', () => {
+                    editField(additionalNotesP, "What additional notes do you have?", item, "notes")
                 })
             }
         }
-        //make a button that removes items from the checklist.
-        const removeFromChecklist = document.createElement("button");
-        removeFromChecklist.textContent = "Remove from checklist";
-        removeFromChecklist.addEventListener('click', () => {
-            removeCheckListItem(checklistUL, item);
-        })
+        
         createItemHeading();
         createItemBodyDisplayButton();
         createDueDateField();
         createPriorityField();
-        createChecklistUL();
         createAdditionalNotes();
         createEditHeaderBtn();
         createDueDateBtn();
         createEditPriorityBtn();
         createAddToChecklistBtn();
         createRemoveFromChecklistBtn();
+        createEditNotesBtn();
     /*
         //console.log(ul);
         //Elements are created the same way its sorted in html

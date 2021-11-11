a5c5bfbf1a4197c9b618163067c6792469dd4636
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-11 07:12:03 -0500

    checklist had a bug where if there were 2 items, you couldnt remove items, but with one you could, thats been fixed

--- src/deleteCheckListItem.js
+++ src/deleteCheckListItem.js
@@ -2,11 +2,23 @@
 function removeCheckListItem (myCheckList, item) {
     const ListItem = window.prompt("what do you want to remove this checklist? (Type it exactly as it appears or it will not work)");
     let Index = item.checklist.indexOf(ListItem);
-    //make a copy of the checklist array without the item we are removing
-    let copy = [
-        ...item.checklist.slice(0, Index - 1),
-        ...item.checklist.slice(Index + 1),
-    ]
+    let copy = [];
+    if(item.checklist.length === 1)
+    {
+        //make a copy of the checklist array without the item we are removing
+         copy = [
+            ...item.checklist.slice(0, Index - 1),
+            ...item.checklist.slice(Index + 1),
+        ]
+    }
+    else
+    {
+         //make a copy of the checklist array without the item we are removing
+          copy = [
+            ...item.checklist.slice(0, Index),
+            ...item.checklist.slice(Index + 1),
+        ]
+    }
     console.log("Copy is " + copy);
     item.checklist = copy.slice(0); //make items checklist = the copy so that it no longer has it in the array
     console.log("Item.checklist is " + item.checklist);
